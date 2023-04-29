68f64a1b0abbc4f551a9f84365583f767aeffad4
WalterVillatoro1204 <wevillatorom@correo.url.edu.gt>
2023-04-28 19:50:58 -0600

    Deserealizar path1, ordenar datos y rejections

--- Lab3_Walter_Villatoro/Program.cs
+++ Lab3_Walter_Villatoro/Program.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Security.Cryptography;
 using System.Text;
+using System.Linq;
 
 namespace Lab3_Walter_Villatoro
 {
@@ -20,9 +21,9 @@ namespace Lab3_Walter_Villatoro
             List<User> Usuario = new List<User>();
             Acciones Actions;
 
-            string direccion = "C:\\Users\\wally\\Desktop\\LAB5\\Lab3_Walter_Villatoro\\bin\\Debug\\netcoreapp3.1\\input_customer_challenge_lab_3.jsonl";
-            string direccion2 = "C:\\Users\\wally\\Desktop\\LAB5\\Lab3_Walter_Villatoro\\bin\\Debug\\netcoreapp3.1\\Costumer.JSONL";
-            using (StreamReader leer = new StreamReader(direccion))
+            string path = "C:\\Users\\wally\\Desktop\\LAB5\\Lab3_Walter_Villatoro\\bin\\Debug\\netcoreapp3.1\\input_customer_challenge_lab_3.jsonl";
+            string path1 = "C:\\Users\\wally\\Desktop\\LAB5\\Lab3_Walter_Villatoro\\bin\\Debug\\netcoreapp3.1\\Costumer.JSONL";
+            using (StreamReader leer = new StreamReader(path))
             {
                 while (!leer.EndOfStream)
                 {
@@ -32,7 +33,52 @@ namespace Lab3_Walter_Villatoro
                     Usuario.Add(persona);
                 }
             }
- 
+            //deserializar los datos de la direccion2
+            using (StreamReader leer = new StreamReader(path1))
+            {
+                string linea = leer.ReadToEnd();
+                Actions = JsonConvert.DeserializeObject<Acciones>(linea);
+            }
+
+            long[] DPI = Enumerable.Repeat(0L, Actions.Customers.Count).ToArray();
+            int[] BUDGETS = Actions.Customers.Select(c => c.Budget).ToArray();
+            int posicion = 0;
+
+            foreach (var item in Actions.Customers)
+            {
+                DPI[posicion] = item.DPI;
+                posicion++;
+            }
+            posicion = 0;
+            foreach (var item in Actions.Customers)
+            {
+                BUDGETS[posicion] = item.Budget;
+                posicion++;
+            }
+            for (int i = 0; i < DPI.Length - 1; i++)
+            {
+                for (int j = 0; j < DPI.Length - (1 + i); j++)
+                {
+                    if (BUDGETS[j] < BUDGETS[j + 1])
+                    {
+                        // Intercambiamos los elementos "BUDGETS" en las posiciones j y j+1
+                        int tempBudget = BUDGETS[j];
+                        BUDGETS[j] = BUDGETS[j + 1];
+                        BUDGETS[j + 1] = tempBudget;
+
+                        // Intercambiamos los elementos "DPI" en las posiciones j y j+1
+                        long tempDPI = DPI[j];
+                        DPI[j] = DPI[j + 1];
+                        DPI[j + 1] = tempDPI;
+                    }
+                }
+            }
+            long Ganador = DPI[Actions.Rejection];
+            int budgetGanador = default;
+            DateTime FechaGanador = new DateTime();
+
+
+
 
         }
     }
